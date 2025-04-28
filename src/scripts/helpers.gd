ab0f33f59a2b8d4ac05c43cb3cfb5a21ed3c4856
laws65 <seanes118@gmail.com>
2025-04-29 00:41:08 +0930

    bump plugin

--- src/scripts/helpers.gd
+++ src/scripts/helpers.gd
@@ -11,31 +11,3 @@ static func get_direction_string_from_angle(angle_rad: float) -> String:
 		return "diagonal_down"
 	else:
 		return "horizontal"
-
-
-static func get_node_props(node: Node, prop_names: Array[String]) -> Dictionary:
-	var out: Dictionary
-	for prop_name in prop_names:
-		var split := prop_name.split(":") as PackedStringArray
-		var node_path := "."
-		var node_prop := ""
-		if split.size() == 1:
-			node_prop = split[0]
-		else:
-			node_path = split[0]
-			node_prop = split[1]
-		out[prop_name] = node.get_node(node_path).get(node_prop)
-	return out
-
-
-static func set_node_props(node: Node, props: Dictionary) -> void:
-	for prop_name in props.keys():
-		var split := prop_name.split(":") as PackedStringArray
-		var node_path := "."
-		var node_prop := ""
-		if split.size() == 1:
-			node_prop = split[0]
-		else:
-			node_path = split[0]
-			node_prop = split[1]
-		node.get_node(node_path).set(node_prop, props[prop_name])
